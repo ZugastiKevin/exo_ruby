1e6161129e236fea63376a6f6cb799d6363dfb08
ZugastiKevin <rochambeaut@gmail.com>
2021-01-12 16:55:00 +0100

    exo finale

--- exo_12.rb
+++ exo_12.rb
@@ -9,7 +9,7 @@ user_years.times do
     user_years -= 1
     age += 1
     if (user_years == age)
-        then puts "Tu avais la moitier de ton age."
+        then puts "Il y a #{user_years} ans, tu avais la moitié de l'âge que tu as aujourd'hui."
         else puts "Il y #{user_years} ans, tu avais #{age} ans."
     end
 end
